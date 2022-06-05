53e68b034ae20d1b5870aacd43c668e47f1fd58f
JSDenton <39029962+JSDenton@users.noreply.github.com>
2022-06-05 23:02:06 +0200

    multihreading 'works' - still some things to do

--- plane.py
+++ plane.py
@@ -22,9 +22,12 @@ class plane(QObject):
     height = 0
     color_counts = [] #holds cell count for every color
     finished = QtCore.pyqtSignal()
-    progress = QtCore.pyqtSignal(int)
+    refresh_func = QtCore.pyqtSignal(int)
+    
+    #def refresh_func(self): pass
+    
 
-    def __init__(self, x=0, y=0, color_num=0):
+    def __init__(self, x:int, y:int, color_num:int):
         super(plane, self).__init__()
         self.width = x
         self.height = y
@@ -32,7 +35,9 @@ class plane(QObject):
         self.color_num = color_num+1
         self.color_counts = [0 for i in range(color_num+1)]
         self.color_counts[0] = x*y
-    
+        self.refresh_func.emit(1)
+        self.neighbourhood_type = 'Random'
+        self.boundary_type = 'Moore'
 
     #checks if this new cell is available and then places it in that spot
     #returns 1 if success, 0 if failure
@@ -41,12 +46,14 @@ class plane(QObject):
         #print(id)
         if(self.space[x][y].id==0):
             self.space[x][y] = cell(id=id)
+            self.color_counts[0]-=1
+            self.color_counts[id]+=1
             return 1
         else: return 0
 
 
 
-    def generate_space(self, nucleon_count, type, window): #generates space with nucleon_count cells
+    def generate_space(self, nucleon_count, type, get_points): #generates space with nucleon_count cells
         i = 0
         print("start generating..")
         #print(type)
@@ -63,14 +70,14 @@ class plane(QObject):
                     self.space[x][y] = cell(id=id)
             case 'Custom':
                 while i<nucleon_count:
-                    x, y = window.get_points() #TODO: create function that gets point with mouseclick
+                    x, y = get_points() #TODO: create function that gets point with mouseclick
                     if x!=-1:
                         i += self.set_new_cell(x, y, i)
         print("end generating..")
     
 
-    def boundaries(self, x, y, boundary_type):
-        if boundary_type=='Periodic':
+    def boundaries(self, x, y):
+        if self.boundary_type=='Periodic':
             x=x%self.width
             y=y%self.height
         else:
@@ -86,22 +93,22 @@ class plane(QObject):
 
     #decides which color gets new cell; type (String) - type of neighbourhood;
     #  boundary_type (int) - type of boundary condition
-    def get_neighbours_color(self, y, x, type, boundary_type): 
+    def get_neighbours_color(self, y, x): 
         neigh_colors = [0 for i in range(self.color_num)]
         
-        match type:
+        match self.neighbourhood_type:
             case 'von Neumann':     
                 for i in range(-2,3):
                     #print(i)
                     if i==0:
                         continue
-                    dx, dy = self.boundaries(x+(i%2), y+(i//2), boundary_type)
+                    dx, dy = self.boundaries(x+(i%2), y+(i//2))
                     if self.space[dx][dy].id!=0: #0 is default cell (not filled with any color)
                         neigh_colors[self.space[dx][dy].id]+=1
             case 'Moore':
                 for i in range(-1,2):
                     for j in range(-1,2):
-                        dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                        dx, dy = self.boundaries(x+i, y+j)
                         if i!=0 and j!=0 and self.space[dx][dy].id!=0:
                             neigh_colors[self.space[dx][dy].id]+=1
             case 'Pentagonal':
@@ -111,25 +118,25 @@ class plane(QObject):
                     case 0: #right side is off
                         for i in range(-1,1):
                             for j in range(-1,2):
-                                dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                                dx, dy = self.boundaries(x+i, y+j)
                                 if i!=0 and j!=0 and self.space[dx][dy].id!=0:
                                     neigh_colors[self.space[dx][dy].id]+=1
                     case 1: #left side is off
                         for i in range(0,2):
                             for j in range(-1,2):
-                                dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                                dx, dy = self.boundaries(x+i, y+j)
                                 if i!=0 and j!=0 and self.space[dx][dy].id!=0:
                                     neigh_colors[self.space[dx][dy].id]+=1
                     case 2: #upper side is off
                         for i in range(-1,2):
                             for j in range(0,2):
-                                dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                                dx, dy = self.boundaries(x+i, y+j)
                                 if i!=0 and j!=0 and self.space[dx][dy].id!=0:
                                     neigh_colors[self.space[dx][dy].id]+=1
                     case 3: #lower side is off
                         for i in range(-1,2):
                             for j in range(-1,1):
-                                dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                                dx, dy = self.boundaries(x+i, y+j)
                                 if i!=0 and j!=0 and self.space[dx][dy].id!=0:
                                     neigh_colors[self.space[dx][dy].id]+=1
 
@@ -139,13 +146,13 @@ class plane(QObject):
                 if hex_type==0:
                     for i in range(-1,2):
                         for j in range(-1,2):
-                            dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                            dx, dy = self.boundaries(x+i, y+j)
                             if i!=0 and j!=0 and (i!=1 and j!=-1) and self.space[dx][dy].id!=0 :
                                 neigh_colors[self.space[dx][dy].id]+=1
                 else:
                     for i in range(-1,2):
                         for j in range(-1,2):
-                            dx, dy = self.boundaries(x+i, y+j, boundary_type)
+                            dx, dy = self.boundaries(x+i, y+j)
                             if i!=0 and j!=0 and (i!=-1 and j!=1) and self.space[dx][dy].id!=0 :
                                 neigh_colors[self.space[dx][dy].id]+=1
 
@@ -164,15 +171,24 @@ class plane(QObject):
         self.color_counts[chosen_color] += 1
         self.color_counts[0] -=1
         
-    def main_loop(self, type, boundaries_type, refresh):
+    def run(self):
         iters = 0
+        print("start main loop...")
         while True:
             for i in range(self.height):
                 for j in range(self.width):
-                    self.get_neighbours_color(i, j, type=type, boundary_type=boundaries_type)
+                    self.get_neighbours_color(i, j)
             if(iters%20==0):
-                refresh()
+                print("refreshing...")
+                self.refresh_func.emit(1)                
             iters+=1
             if self.color_counts[0]<=0: #stop criterium - when there's no more white "tiles"
                 break
+        print("finished...")
         self.finished.emit()
+
+    def send_args(self, type, boundaries_type, refresh):
+        self.neighbourhood_type = type
+        self.boundary_type = boundaries_type
+        self.refresh_func = refresh
+        print("args_sent")
